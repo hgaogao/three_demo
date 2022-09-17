2c7aa93a83d1eb28678f02a220097b530cf011fd
hgaogaogao <hgaogaogao@gmail.com>
2022-09-18 00:29:12 +0800

    init

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+		node: true,
+	},
+	extends: [
+		'plugin:vue/vue3-recommended',
+		'eslint:recommended',
+		'plugin:prettier/recommended',
+	],
+	overrides: [],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['vue', 'prettier'],
+	rules: {
+		indent: ['error', 'tab'],
+	},
+}
